a6154d0c234b431931fbabee15cfb7305971fd6e
Hebbelmann <123360231+RHebbelmann@users.noreply.github.com>
2025-01-20 10:00:43 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    if (input.soundLevel() > 20) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(5000)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
